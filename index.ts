7941cc677a9ced291d5b23d1765e7294772bdd84
iamCelott <dcviriya313@gmail.com>
2024-10-29 23:19:27 +0800

    init

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import { createServer } from "http";
+import path from "path";
+import { Server } from "socket.io";
+
+const port = 5000;
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("New Web Socket Connection...");
+
+  socket.emit("message", "Welcome to the Chatroom!");
+});
+
+httpServer.listen(port, function () {
+  console.log(`Server running on http://localhost:${port}`);
+});
