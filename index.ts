a91a98e3ca8c533fab09761938461c71870b378b
iamCelott <Dcviriya313@gmail.com>
2024-10-30 12:35:59 +0800

    create join function

--- index.ts
+++ index.ts
@@ -8,6 +8,8 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
@@ -16,10 +18,26 @@ app.get("/chat", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "views", "chat.html"));
 });
 
+app.post("/message", async (req: Request, res: Response) => {
+  const { username, message } = req.body;
+
+  if (!username || !message) {
+    res.status(400).json({ error: "Username and message are required" });
+  }
+
+  await io.emit("message", { username, message });
+
+  res.status(200).json({ status: "Message sent" });
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcome to the Chatroom!");
+  socket.emit("welcome", "Welcome Guachamole!!");
+
+  socket.on("join", (username) => {
+    socket.broadcast.emit("user_join", `${username} has joined the chat`);
+  });
 });
 
 httpServer.listen(port, function () {
